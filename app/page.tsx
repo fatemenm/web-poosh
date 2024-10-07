f80a8adadc8cece9a134324380aae0071f3a03d0
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-10-07 09:37:48 +0330

    feat: initialize root page and carousel components, and add categories names data

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Carousel from "./ui/carousel ";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <Carousel />
+    </div>
+  );
+}
---
+++ app/ui/carousel .tsx
@@ -0,0 +1,7 @@
+import { categoriesNames } from "../lib/placeholder-data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Carousel() {
+  return <div></div>;
+}
