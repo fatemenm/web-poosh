5f35341d196ddb92a5b9e25cba053b55ba33d077
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-10-19 16:10:12 +0330

    chore: add Banner and NavbarItem types

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,16 +1,32 @@
-export type User = {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-};
+// export type User = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   password: string;
+// };
 
-export type product = {
-  name: string;
-  category: string;
-  subCategory: string | undefined;
-  color: string;
-  size: string;
-  price: string;
-  stockStatus: boolean;
+// export type product = {
+//   name: string;
+//   category: string;
+//   subCategory: string | undefined;
+//   color: string;
+//   size: string;
+//   price: string;
+//   stockStatus: boolean;
+// };
+
+export type Banner = {
+  startDate: string;
+  endDate: string;
+  dynamicText: string;
+  staticText: string;
+  ctaText: string;
+  ctaUrl: string;
+};
+// TODO: how to replace null with sth else?
+export type NavbarItem = {
+  linkText: string;
+  linkUrl: string | null;
+  subLinks: Object;
+  isExpandable: boolean;
 };
