186b226bf57fa8e3ed32c1d7db1456e748b32d0e
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-03 12:12:03 +0330

    refactor: add search params with url interface

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,14 +3,12 @@ import { Banner, NavbarItem, responseBody } from "./definitions";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const urls = {
   getBanners: new URL("/api/banners", apiUrl),
-  getNavbarItems: new URL(
-    "/api/navbar-items?sort=order:desc&populate=image",
-    apiUrl
-  ),
+  getNavbarItems: new URL("/api/navbar-items", apiUrl),
 };
+
 export async function getBannerData() {
   try {
-    const response = await fetch(urls.getBanners.href, { cache: "no-store" });
+    const response = await fetch(urls.getBanners.href);
     const body: responseBody = await response.json();
     const data = body.data.map((item) => ({
       ...item,
@@ -22,7 +20,7 @@ export async function getBannerData() {
       .filter((banner) => {
         return banner.startDate < now && banner.endDate > now && banner;
       })
-      .sort((a, b) => b.endDate.getTime() - a.endDate.getTime())
+      .sort((a, b) => a.startDate.getTime() - b.startDate.getTime())
       .pop();
     return earliestActiveBanner;
   } catch (error) {
@@ -31,9 +29,9 @@ export async function getBannerData() {
 }
 export async function getNavbarItems() {
   try {
-    const response = await fetch(urls.getNavbarItems.href, {
-      cache: "no-store",
-    });
+    urls.getNavbarItems.searchParams.append("sort", "order:desc");
+    urls.getNavbarItems.searchParams.append("populate", "image");
+    const response = await fetch(urls.getNavbarItems.href);
     const body: responseBody = await response.json();
     const data = body.data as NavbarItem[];
     return data;
@@ -41,46 +39,3 @@ export async function getNavbarItems() {
     console.log(error);
   }
 }
-
-// interface Object {
-//   flatten(data: any): FlattenedObject;
-// }
-
-// interface FlattenedObject {
-//   [key: string]: any;
-// }
-
-// Object.defineProperty(Object.prototype, "flatten", {
-//   value: function (data: any): FlattenedObject {
-//     const result: FlattenedObject = {};
-
-//     function recurse(cur: any, prop: string) {
-//       if (Object(cur) !== cur) {
-//         result[prop] = cur;
-//       } else if (Array.isArray(cur)) {
-//         for (let i = 0, l = cur.length; i < l; i++) {
-//           recurse(cur[i], `${prop}[${i}]`);
-//         }
-//         if (cur.length === 0) {
-//           result[prop] = [];
-//         }
-//       } else {
-//         let isEmpty = true;
-//         for (const p in cur) {
-//           if (cur.hasOwnProperty(p)) {
-//             isEmpty = false;
-//             recurse(cur[p], prop ? `${prop}.${p}` : p);
-//           }
-//         }
-//         if (isEmpty && prop) {
-//           result[prop] = {};
-//         }
-//       }
-//     }
-
-//     recurse(data, "");
-//     return result;
-//   },
-//   writable: true,
-//   configurable: true,
-// });
