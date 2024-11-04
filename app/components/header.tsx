91a125ff36c7c609ac3160e2292de22ec03d18b6
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-04 14:17:16 +0330

    feat: implement showing navbar item Menu in a grid

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,6 +6,7 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { create } from "domain";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -29,18 +30,37 @@ export default function Header({
   function handleLinkHover(item: NavbarItem | null) {
     setHoveredLinkData(item);
   }
-
   useEffect(() => {
     setNavbarItems(navbarItemsData);
     setBanner(bannerData);
   }, []);
+  //define a function to create a grid of sub links
+  function createSublinkGrid(items: NavigationLink[] | undefined) {
+    if (!items) return null;
+    // find the number of cols
+    items.sort((a, b) => a.col - b.col);
+    const colCount = items[items.length - 1].col + 1;
+    // generate a grid of items based on rows and cols
+    let grid = [];
+    for (let i = 0; i < colCount; i++) {
+      grid.push(items.filter((item) => item.col === i));
+    }
+    return grid;
+  }
+
+  console.log(
+    "height:",
+    hoveredLinkData?.image?.height,
+    " width:",
+    hoveredLinkData?.image?.width
+  );
 
   return (
     <header className="flex flex-col">
       {banner ? (
         <PromotionalBanner data={banner} />
       ) : (
-        <div className="bg-stone-800 w-full p-2 flex flex-row justify-start text-white text-sm font-light min-h-11 box-border"></div>
+        <div className="bg-stone-800 w-full p-2 flex flex-row justify-start text-white text-sm font-light min-h-10 box-border"></div>
       )}
       <div className="flex flex-row justify-between items-center mx-48 ">
         <div className="flex flex-row justify-between gap-8">
@@ -66,29 +86,46 @@ export default function Header({
         </div>
       </div>
       {hoveredLinkData?.isExpandable && (
-        <div className="absolute top-24 w-screen">
-          <div className="bg-stone-100 flex flex-row justify-center py-8">
+        <div className="absolute top-24 w-screen ">
+          <div
+            className="bg-stone-100 flex flex-row justify-center py-5"
+            onMouseEnter={() =>
+              setHoveredLinkData({ ...hoveredLinkData, isExpandable: true })
+            }
+            onMouseLeave={() =>
+              setHoveredLinkData({ ...hoveredLinkData, isExpandable: false })
+            }
+          >
             <div className="flex flex-row justify-between w-2/3 px-6">
-              {hoveredLinkData.image && (
+              {hoveredLinkData.image ? (
                 <Image
                   src={`${process.env.NEXT_PUBLIC_API_URL}${hoveredLinkData.image?.url}`}
                   alt={hoveredLinkData.image?.alternativeText}
                   width={hoveredLinkData.image.width}
                   height={hoveredLinkData.image.height}
                 />
+              ) : (
+                <div></div>
               )}
-              <div className="flex flex-row">
-                <ul>
-                  {hoveredLinkData.subLinks?.items.map(
-                    (subLink: NavigationLink, index: number) => {
-                      return (
-                        <li key={index}>
-                          <a href={subLink.linkUrl}>{subLink.linkText}</a>
-                        </li>
-                      );
-                    }
-                  )}
-                </ul>
+              <div className="flex flex-row-reverse gap-20">
+                {createSublinkGrid(hoveredLinkData?.subLinks?.items)?.map(
+                  (col, index) => {
+                    return (
+                      <ul
+                        key={index}
+                        className="text-right text-stone-600 font-normal text-sm flex flex-col gap-5"
+                      >
+                        {col.map((item, index) => {
+                          return (
+                            <li key={index}>
+                              <a href={item.linkUrl}>{item.linkText}</a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    );
+                  }
+                )}
               </div>
             </div>
           </div>
