3b6c1c977df7b457cecff79fe32a6c2d0dae091f
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-10 11:49:46 +0330

    refactor: remove usage of persian-number package, use tailwind css variable fonts

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,15 +6,12 @@ import {
   faTelegram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Roboto } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import PN from "persian-number";
 
 import logo from "../../public/logo-footer.svg";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 const footerContent = (
   <div className="flex flex-col gap-10 text-right py-5">
     <div className="flex flex-col gap-2">
@@ -22,29 +19,27 @@ const footerContent = (
         فروشگاه اینترنتی پوشاک و لباس مردانه وب پوش
       </h1>
       <p className="text-xs leading-5 text-stone-800 font-light" dir="rtl">
-        فروشگاه لباس مردانه وب پوش از سال {PN.convertEnToPe(1393)} با هدف ایجاد
-        بستری مناسب و مطمئن و نیز راحتی هرچه بیشتر مشتریان، برای خرید حضوری و
-        غیرحضوری پوشاک مردانه شروع به فعالیت نمود. ما با ارائه انواع البسه
-        مردانه نظیر: انواع <strong>پیراهن مردانه</strong> (پیراهن های رسمی، یقه
-        دپلمات، آستین کوتاه، چهارخانه، جین و...)، انواع{" "}
-        <strong>شلوار مردانه</strong> (شلوارهای پارچه ای، کتان، جین واسلش)،
-        انواع <strong>تیشرت</strong> و <strong> پولوشرت </strong>و انواع{" "}
+        فروشگاه لباس مردانه وب پوش از سال ۱۳۹۳ با هدف ایجاد بستری مناسب و مطمئن
+        و نیز راحتی هرچه بیشتر مشتریان، برای خرید حضوری و غیرحضوری پوشاک مردانه
+        شروع به فعالیت نمود. ما با ارائه انواع البسه مردانه نظیر: انواع{" "}
+        <strong>پیراهن مردانه</strong> (پیراهن های رسمی، یقه دپلمات، آستین
+        کوتاه، چهارخانه، جین و...)، انواع <strong>شلوار مردانه</strong>{" "}
+        (شلوارهای پارچه ای، کتان، جین واسلش)، انواع <strong>تیشرت</strong> و{" "}
+        <strong> پولوشرت </strong>و انواع{" "}
         <strong>کت، جلیقه، سویشرت، کاپشن، پلیور</strong> و ... همچنین انواع{" "}
         <strong>کفش های مردانه</strong> (کفش های رسمی، روزمره و ورزشی) و اکسسوری
         هایی نظیر <strong>کمربند، کلاه</strong> و... به دنبال ارائه تجربه ای
-        راحت و لذت بخش از <strong>خرید اینترنتی</strong>
-        برای مشتریان می باشیم. مجموعه وب پوش ارائه{" "}
+        راحت و لذت بخش از <strong>خرید اینترنتی </strong> برای مشتریان می باشیم.
+        مجموعه وب پوش ارائه
         <strong>محصولات با کیفیت</strong> را در دستور کار خود قرار داده است. از
         این رو تمامی محصولات ارائه شده در سایت و فروشگاه های وب پوش از محصولات
         با کیفیت می باشند. برای اطمینان خاطر مشتریان، تیم پشتیبانی وب پوش شنبه
-        تا پنجشنبه از ساعت {PN.convertEnToPe(9)} الی {PN.convertEnToPe(17)}{" "}
-        آماده راهنمایی و پاسخگویی به سوالات شما مشتریان عزیز می باشند. همچنین
-        جهت رفاه هرچه بیشتر مشتریان پنج فروشگاه حضوری وب پوش (فروشگاه های
-        اقدسیه، شهرک غرب، ایران‌مال
+        تا پنجشنبه از ساعت ۹ الی ۱۷ آماده راهنمایی و پاسخگویی به سوالات شما
+        مشتریان عزیز می باشند. همچنین جهت رفاه هرچه بیشتر مشتریان پنج فروشگاه
+        حضوری وب پوش (فروشگاه های اقدسیه، شهرک غرب، ایران‌مال
         <span dir="ltr"> G1 </span>، ایران‌مال
-        <span dir="ltr"> G2 </span>و هدیش مال) هر روز هفته از ساعت{" "}
-        {PN.convertEnToPe(11)} صبح الی {PN.convertEnToPe(10)} شب آماده خدمت
-        رسانی به مشتریان می باشد.
+        <span dir="ltr"> G2 </span>و هدیش مال) هر روز هفته از ساعت ۱۱ صبح الی ۱۰
+        شب آماده خدمت رسانی به مشتریان می باشد.
       </p>
     </div>
     <div className="flex flex-col gap-2">
@@ -134,11 +129,7 @@ export default function Footer() {
               <nav>
                 <ul className="flex flex-col text-sm font-light items-end gap-2">
                   <li>
-                    <Link href="">
-                      پشتیبانی: {PN.convertEnToPe(91005005)} -
-                      {PN.convertEnToPe(0)}
-                      {PN.convertEnToPe(21)}
-                    </Link>
+                    <Link href="">پشتیبانی: ۹۱۰۰۵۰۰۵ - ۰۲۱</Link>
                   </li>
                   <li>
                     <Link href="">سوالات متداول</Link>
@@ -188,7 +179,7 @@ export default function Footer() {
       </div>
       {isHomePage && footerContent}
       <div className="flex flex-row justify-between items-center text-stone-700">
-        <small className={`font-semibold text-xs ${roboto.className}`}>
+        <small className="font-semibold text-xs font-english">
           webpoosh.com - 2024 © Copyright
         </small>
         <small className="font-light text-xs">
