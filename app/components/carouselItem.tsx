4beaf53b6d3672105144f564146a1734b988e257
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-17 15:40:39 +0330

    feature: create first version of carousel with useRef hook

--- app/components/carouselItem.tsx
+++ app/components/carouselItem.tsx
@@ -1,21 +1,20 @@
-// import Image from "next/image";
+import Image from "next/image";
 
-// export default function CarouselItem({
-//   image,
-//   imageType,
-// }: {
-//   image: string;
-//   imageType: string;
-// }) {
-//   return (
-//     <div>
-//       <Image
-//         src={`/images/${imageType}/${image}`}
-//         alt={image}
-//         width={342}
-//         height={432}
-//         className="max-w-fit"
-//       />
-//     </div>
-//   );
-// }
+import { Category } from "@/lib/definitions";
+
+import { apiBaseUrl } from "../../config";
+
+export default function CarouselItem({ item }: { item: Category }) {
+  return (
+    <div className="flex flex-col items-center ">
+      <Image
+        src={`${apiBaseUrl}${item.image.url}`}
+        alt={item.image.alternativeText}
+        width={item.image.width}
+        height={item.image.height}
+        className="max-w-fit"
+      />
+      <div className="underline underline-offset-8 ">{item.name}</div>
+    </div>
+  );
+}
