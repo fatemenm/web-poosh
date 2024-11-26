223f3037402e8c22788791d8bd046d1a2e8f3f9e
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-11-26 10:55:06 +0330

    refactor: wrap icons in spans to prevent layout shift

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -3,18 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function BreadCrumb() {
+  // TODO: fix icons layout shift problem
   return (
     <div className="flex flex-row-reverse items-center gap-4 bg-stone-100 px-10 py-3 text-sm text-stone-700">
       <Link href="">وب پوش</Link>
-      <FontAwesomeIcon
-        icon={faChevronLeft}
-        style={{ fontSize: 12, color: "gray" }}
-      />
+      <span>
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          style={{ fontSize: 12, color: "gray" }}
+        />
+      </span>
       <Link href="">لباس</Link>
-      <FontAwesomeIcon
-        icon={faChevronLeft}
-        style={{ fontSize: 12, color: "gray" }}
-      />
+      <span>
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          style={{ fontSize: 12, color: "gray" }}
+        />
+      </span>
       <Link href="">شلوار جین کارگو ۱۲۸۱۶</Link>
     </div>
   );
