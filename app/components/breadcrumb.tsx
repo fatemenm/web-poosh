180d0509537a15aaf75a02a19999b0df2a2e25b6
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-12-03 12:55:25 +0330

    feat: make breadcrumb interactive

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -2,25 +2,29 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-export default function BreadCrumb() {
-  // TODO: fix icons layout shift problem
+type BreadCrumbItem = {
+  label: string;
+  href: string;
+};
+
+export default function BreadCrumb({ items }: { items: BreadCrumbItem[] }) {
   return (
     <div className="flex w-10/12 flex-row items-center gap-4 bg-stone-100 px-10 py-3 text-sm text-stone-700">
-      <Link href="">وب پوش</Link>
-      <span>
-        <FontAwesomeIcon
-          icon={faChevronLeft}
-          style={{ fontSize: 12, color: "gray" }}
-        />
-      </span>
-      <Link href="">لباس</Link>
-      <span>
-        <FontAwesomeIcon
-          icon={faChevronLeft}
-          style={{ fontSize: 12, color: "gray" }}
-        />
-      </span>
-      <Link href="">شلوار جین کارگو ۱۲۸۱۶</Link>
+      {items.map((item, index) => {
+        return (
+          <Link href={item.href} key={index} className="flex flex-row gap-4">
+            {item.label}
+            {items.length - 1 !== index && (
+              <span>
+                <FontAwesomeIcon
+                  icon={faChevronLeft}
+                  style={{ fontSize: 12, color: "gray" }}
+                />
+              </span>
+            )}
+          </Link>
+        );
+      })}
     </div>
   );
 }
