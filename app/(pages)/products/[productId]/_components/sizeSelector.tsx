ffa7af0297185de08df2adc4c27e1c3c3c0325ed
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-12-16 10:11:25 +0330

    fix: use unique id for input size instead of index

--- app/(pages)/products/[productId]/_components/sizeSelector.tsx
+++ app/(pages)/products/[productId]/_components/sizeSelector.tsx
@@ -10,23 +10,23 @@ export default function SizeSelector({ sizes }: { sizes: string[] }) {
       className="flex flex-row flex-wrap items-center"
       onChange={(e) => setSelectedSize((e.target as HTMLInputElement).value)}
     >
-      {sizes.map((item, index) => {
+      {sizes.map((size, index) => {
         return (
           <label
             key={index}
-            htmlFor={index.toString()}
-            className="flex h-9 w-14 flex-row items-center justify-center border border-x-0 border-stone-500 pt-0.5 text-sm text-stone-500 odd:border-x hover:bg-stone-800 hover:text-stone-50 has-[:checked]:bg-stone-800 has-[:checked]:text-stone-50"
+            htmlFor={size}
+            className="flex h-9 w-14 cursor-pointer flex-row items-center justify-center border border-x-0 border-stone-500 pt-0.5 text-sm text-stone-500 odd:border-x hover:bg-stone-800 hover:text-stone-50 has-[:checked]:bg-stone-800 has-[:checked]:text-stone-50"
           >
             <input
               readOnly
               type="radio"
-              id={index.toString()}
-              value={item}
+              id={size}
+              value={size}
               name="size"
               className="appearance-none"
-              checked={selectedSize === item}
+              checked={selectedSize === size}
             />
-            <span>{item}</span>
+            <span>{size}</span>
           </label>
         );
       })}
