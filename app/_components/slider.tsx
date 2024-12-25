a99b93e73bc07cf7f47b3d041b297bb0a4a9a5e2
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-12-25 16:06:38 +0330

    refactor: set default value for containerClass to an empty string

--- app/_components/slider.tsx
+++ app/_components/slider.tsx
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 export default function Slider({
-  containerClass,
+  containerClass = "",
   customSetting,
   children,
 }: {
