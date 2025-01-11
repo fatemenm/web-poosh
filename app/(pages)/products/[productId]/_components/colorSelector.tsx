49b71dd4378ef4dd37da32636d83a7dd7c5f1aae
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-01-11 15:25:21 +0330

    refactor: move selected color display to ProductDetails for better separation of concerns, and rename state variable names in ProductDetails component

--- app/(pages)/products/[productId]/_components/colorSelector.tsx
+++ app/(pages)/products/[productId]/_components/colorSelector.tsx
@@ -1,48 +1,45 @@
 "use client";
 
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
-import { useState } from "react";
 import React from "react";
 
-const defaultColor = "آبی روشن";
 export default function ColorSelector({
   colors,
+  onSelect,
+  defaultColor,
 }: {
-  colors: Array<{ name: string; colorCode: string; isAvailable: boolean }>;
+  colors: Array<{
+    name: string;
+    colorCode: string;
+    isAvailable: boolean;
+  }>;
+  onSelect: (value: string) => void;
+  defaultColor: string;
 }) {
-  const [selectedColor, setSelectedColor] = useState<string>(defaultColor);
-
   return (
-    <div className="flex flex-col gap-6 text-stone-800">
-      <div className="text-sm font-light">
-        رنگ انتخابی شما:
-        <span className="pr-1 font-normal">{selectedColor}</span>
-      </div>
-      <ToggleGroup.Root
-        className="flex flex-row-reverse flex-wrap gap-3"
-        onValueChange={(value) => setSelectedColor(value)}
-        type="single"
-        defaultValue={defaultColor}
-        aria-label="رنگ محصول"
-      >
-        {colors.map((color, index) => {
-          return (
-            <ToggleGroup.Item
-              value={color.name}
-              key={index}
-              className="relative rounded-full border-[2px] bg-white p-[3px] hover:border-black data-[state=on]:border-black"
-            >
-              <div
-                style={{ backgroundColor: color.colorCode }}
-                className="h-8 w-8 rounded-full"
-              />
-              {!color.isAvailable && (
-                <div className="absolute left-[18px] top-0 h-10 w-0.5 -rotate-45 bg-stone-400" />
-              )}
-            </ToggleGroup.Item>
-          );
-        })}
-      </ToggleGroup.Root>
-    </div>
+    <ToggleGroup.Root
+      className="flex flex-row-reverse flex-wrap gap-3"
+      onValueChange={(value) => onSelect(value)}
+      type="single"
+      defaultValue={defaultColor}
+      aria-label="رنگ محصول"
+    >
+      {colors.map((color, index) => {
+        return (
+          <ToggleGroup.Item
+            value={color.name}
+            key={index}
+            style={{
+              color: color.colorCode,
+            }}
+            className="relative h-10 w-10 rounded-full border-2 bg-white p-2 before:absolute before:inset-0 before:top-0 before:rounded-full before:border-2 before:border-white before:bg-current hover:border-2 hover:border-black data-[state=on]:border-2 data-[state=on]:border-black"
+          >
+            {!color.isAvailable && (
+              <div className="absolute left-[16px] top-0 h-[38px] w-0.5 -rotate-45 bg-stone-400" />
+            )}
+          </ToggleGroup.Item>
+        );
+      })}
+    </ToggleGroup.Root>
   );
 }
--- app/(pages)/products/[productId]/_components/productDetails.tsx
+++ app/(pages)/products/[productId]/_components/productDetails.tsx
@@ -26,22 +26,40 @@ const sizes = [
   { value: "34", isAvailable: true },
   { value: "35", isAvailable: true },
 ];
+const defaultColor = "آبی روشن";
 
 export default function ProductDetails({
   product,
 }: {
   product: ClotheProduct;
 }) {
-  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+  const [isProductAvailable, setIsProductAvailable] = useState<boolean>(false);
+  const [selectedColor, setSelectedColor] = useState<string>(defaultColor);
+  console.log(setIsProductAvailable);
   return (
     <div className="flex flex-row justify-end gap-10">
       <div className="block w-5/12">
         <GallerySlider images={product.images} isExpandable={true} />
       </div>
       <div className="ml-32 flex w-3/12 flex-col gap-6 text-right">
-        <ProductHeader {...product} />
+        <ProductHeader
+          name={product.name}
+          price={product.price}
+          id={product.id}
+        />
         <hr />
-        <ColorSelector colors={colors} />
+        <div className="flex flex-col gap-6 text-stone-800">
+          <div className="text-sm font-light">
+            رنگ انتخابی شما:
+            <span className="pr-1 font-normal">{selectedColor}</span>
+          </div>
+          <ColorSelector
+            colors={colors}
+            onSelect={setSelectedColor}
+            defaultColor={defaultColor}
+          />
+        </div>
+
         <SizeSelector sizes={sizes} />
         <SizeGuide images={product.images} />
         <span className="text-sm font-light text-stone-800">
@@ -49,10 +67,10 @@ export default function ProductDetails({
         </span>
         <button
           onClick={() => console.log("clicked")}
-          disabled={isButtonDisabled}
-          className={`py-5 text-sm text-white ${isButtonDisabled ? "bg-neutral-400 hover:bg-neutral-400" : "bg-green-700 hover:bg-green-800"}`}
+          disabled={isProductAvailable}
+          className={`py-5 text-sm text-white ${isProductAvailable ? "bg-neutral-400 hover:bg-neutral-400" : "bg-green-700 hover:bg-green-800"}`}
         >
-          {isButtonDisabled ? "ناموجود" : "اضافه به سبد خرید"}
+          {isProductAvailable ? "ناموجود" : "اضافه به سبد خرید"}
         </button>
 
         <button
@@ -60,11 +78,11 @@ export default function ProductDetails({
           className="mt-4 flex flex-row justify-between px-4 py-3 text-sm text-stone-800 outline outline-1 hover:bg-stone-800 hover:text-stone-50"
         >
           <span>
-            <FontAwesomeIcon icon={faLocationDot} style={{ fontSize: 20 }} />
+            <FontAwesomeIcon icon={faLocationDot} className="text-[20px]" />
           </span>
           مشاهده موجودی در فروشگاه حضوری
           <span>
-            <FontAwesomeIcon icon={faChevronLeft} style={{ fontSize: 20 }} />
+            <FontAwesomeIcon icon={faChevronLeft} className="text-[20px]" />
           </span>
         </button>
       </div>
