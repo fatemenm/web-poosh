01a5beacd2ea0030833bab21816418b6133457c1
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-01-11 15:22:26 +0330

    refactor: reverse product validity check to reduce indentation

--- app/(pages)/products/[productId]/page.tsx
+++ app/(pages)/products/[productId]/page.tsx
@@ -16,65 +16,60 @@ export async function generateStaticParams() {
     productId: product.documentId,
   }));
 }
-
+const breadcrumbItems = [
+  {
+    label: "وب پوش",
+    href: "/",
+  },
+  {
+    label: "لباس",
+    href: "/products",
+  },
+  {
+    label: "شلوار جین کارگو ۴۵",
+    href: ".",
+  },
+];
 export default async function Product({
   params,
 }: {
   params: Promise<{ productId: string }>;
 }) {
   const product = await getClotheProductById((await params).productId);
-  const links = [
-    {
-      label: "وب پوش",
-      href: "/",
-    },
-    {
-      label: "لباس",
-      href: "/products",
-    },
-    {
-      label: "شلوار جین کارگو ۴۵",
-      href: ".",
-    },
-  ];
+  if (!product) throw new Error("product is undefined");
   const sliderContent: ClotheProduct[] = new Array(10).fill(product);
 
-  const customSetting = {
-    infinite: false,
-    slidesToScroll: 5,
-    slidesToShow: 5,
-  };
-
-  if (product)
-    return (
-      <div className="mx-auto flex w-10/12 flex-col gap-20">
-        <BreadCrumb items={links} />
-        {product && <ProductDetails product={product} />}
-        <ProductDescription product={product} />
-        <div className="mb-20 flex w-full flex-col gap-4">
-          <span className="text-lg">محصولات مشابه دیگر</span>
-          <BasicSlider sliderSetting={customSetting} containerClass="">
-            {sliderContent.map((item) => (
-              <Link
-                href="/"
-                key={item.id}
-                className="flex cursor-pointer flex-col items-center outline-none"
-              >
-                <Image
-                  src={apiBaseUrl + item.images[0].url}
-                  alt={item.images[0].alternativeText}
-                  width={item.images[0].width}
-                  height={item.images[0].height}
-                  quality={100}
-                />
-                <div className="mt-4 flex justify-center gap-1 text-center text-sm font-light text-stone-700">
-                  <span> تومان</span>
-                  {Number(item.price.replace(/,/g, "")).toLocaleString("fa-IR")}
-                </div>
-              </Link>
-            ))}
-          </BasicSlider>
-        </div>
+  return (
+    <div className="mx-auto flex w-10/12 flex-col gap-20">
+      <BreadCrumb items={breadcrumbItems} />
+      <ProductDetails product={product} />
+      <ProductDescription product={product} />
+      <div className="mb-20 flex w-full flex-col gap-4">
+        <span className="text-lg">محصولات مشابه دیگر</span>
+        <BasicSlider
+          setting={{ infinite: false, slidesToScroll: 5, slidesToShow: 5 }}
+        >
+          {sliderContent.map((item) => (
+            <Link
+              href="/"
+              key={item.id}
+              className="flex cursor-pointer flex-col items-center outline-none"
+            >
+              <Image
+                src={apiBaseUrl + item.images[0].url}
+                alt={item.images[0].alternativeText}
+                width={item.images[0].width}
+                height={item.images[0].height}
+                quality={100}
+              />
+              <div className="mt-4 flex justify-center gap-1 text-center text-sm font-light text-stone-700">
+                <span> تومان</span>
+                {Number(item.price.replace(/,/g, "")).toLocaleString("fa-IR")}
+              </div>
+            </Link>
+          ))}
+        </BasicSlider>
       </div>
-    );
+    </div>
+  );
 }
