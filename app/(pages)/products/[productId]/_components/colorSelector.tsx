0be6e86cb6defa72ee6095002fd86e83071603b6
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-01-21 10:04:17 +0330

    feat: add dynamic size/color selectors, refactor sizeSelector styles, update gallerySlider images by color change

--- app/(pages)/products/[productId]/_components/colorSelector.tsx
+++ app/(pages)/products/[productId]/_components/colorSelector.tsx
@@ -6,22 +6,26 @@ import React from "react";
 export default function ColorSelector({
   colors,
   onSelect,
-  defaultColor,
+  selectedColor,
 }: {
-  colors: Array<{
+  colors: {
     name: string;
-    colorCode: string;
+    colorHex: string;
     isAvailable: boolean;
-  }>;
+  }[];
+  selectedColor: string;
   onSelect: (value: string) => void;
-  defaultColor: string;
 }) {
   return (
     <ToggleGroup.Root
       className="flex flex-row-reverse flex-wrap gap-3"
-      onValueChange={(value) => onSelect(value)}
+      value={selectedColor}
+      onValueChange={(value: string) => {
+        if (value === "") onSelect(selectedColor);
+        else onSelect(value);
+      }}
       type="single"
-      defaultValue={defaultColor}
+      defaultValue={selectedColor}
       aria-label="رنگ محصول"
     >
       {colors.map((color, index) => {
@@ -30,7 +34,7 @@ export default function ColorSelector({
             value={color.name}
             key={index}
             style={{
-              color: color.colorCode,
+              color: color.colorHex,
             }}
             className="relative h-10 w-10 rounded-full border-2 bg-white p-2 before:absolute before:inset-0 before:top-0 before:rounded-full before:border-2 before:border-white before:bg-current hover:border-2 hover:border-black data-[state=on]:border-2 data-[state=on]:border-black"
           >
--- app/(pages)/products/[productId]/_components/productDetails.tsx
+++ app/(pages)/products/[productId]/_components/productDetails.tsx
@@ -1,50 +1,65 @@
 "use client";
 
-import {
-  faChevronLeft,
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
 import { useState } from "react";
 
 import GallerySlider from "@/_components/gallerySlider";
-import { ClotheProduct } from "@/_lib/definitions";
+import { Product } from "@/_lib/definitions";
 
 import ColorSelector from "./colorSelector";
 import { ProductHeader } from "./productHeader";
 import SizeGuide from "./sizeGuide";
 import SizeSelector from "./sizeSelector";
 
-const colors = [
-  { name: "آبی روشن", colorCode: "#90b3de", isAvailable: true },
-  { name: "خاکستری", colorCode: "#808080", isAvailable: false },
-];
-const sizes = [
-  { value: "31", isAvailable: true },
-  { value: "32", isAvailable: true },
-  { value: "33", isAvailable: false },
-  { value: "34", isAvailable: true },
-  { value: "35", isAvailable: true },
-];
-const defaultColor = "آبی روشن";
+export default function ProductDetails({ product }: { product: Product }) {
+  const [selectedColor, setSelectedColor] = useState<string>(
+    product.colors[0].name
+  );
+  const [selectedSize, setSelectedSize] = useState<string>();
 
-export default function ProductDetails({
-  product,
-}: {
-  product: ClotheProduct;
-}) {
-  const [isProductAvailable, setIsProductAvailable] = useState<boolean>(false);
-  const [selectedColor, setSelectedColor] = useState<string>(defaultColor);
-  console.log(setIsProductAvailable);
+  function hasStockBy(type: "color" | "size") {
+    return product.stocks.reduce(
+      (acc, currStock) => {
+        acc[currStock[type]] ||= Boolean(currStock.quantity);
+        return acc;
+      },
+      {} as Record<string, boolean>
+    );
+  }
+  function getColors() {
+    const colorAvailabilityMap = hasStockBy("color");
+    return product.colors.map((color) => ({
+      ...color,
+      isAvailable: colorAvailabilityMap[color.name] || false,
+    }));
+  }
+  function getSizes() {
+    const sizeAvailabilityMap = hasStockBy("size");
+    return product.sizes.map((size) => ({
+      value: size,
+      isAvailable: sizeAvailabilityMap[size] || false,
+    }));
+  }
+  const colors = getColors();
+  const sizes = getSizes();
+  const isProductAvailable = !product.stocks.every(
+    (variant) => variant.quantity === 0
+  );
   return (
     <div className="flex flex-row justify-end gap-10">
       <div className="block w-5/12">
-        <GallerySlider images={product.images} isExpandable={true} />
+        <GallerySlider
+          images={
+            product.imagesByColor.find((item) => item.color === selectedColor)
+              ?.images
+          }
+          isExpandable={true}
+        />
       </div>
       <div className="ml-32 flex w-3/12 flex-col gap-6 text-right">
         <ProductHeader
           name={product.name}
-          price={product.price}
+          price={product.basePrice}
           id={product.id}
         />
         <hr />
@@ -54,36 +69,39 @@ export default function ProductDetails({
             <span className="pr-1 font-normal">{selectedColor}</span>
           </div>
           <ColorSelector
+            selectedColor={selectedColor}
             colors={colors}
             onSelect={setSelectedColor}
-            defaultColor={defaultColor}
           />
         </div>
 
-        <SizeSelector sizes={sizes} />
-        <SizeGuide images={product.images} />
+        <SizeSelector sizes={sizes} onSelect={setSelectedSize} />
+        <SizeGuide images={product.imagesByColor[0].images} />
         <span className="text-sm font-light text-stone-800">
           ارسال رایگان برای خرید بالای ۲,۰۰۰,۰۰۰ تومان
         </span>
         <button
-          onClick={() => console.log("clicked")}
-          disabled={isProductAvailable}
-          className={`py-5 text-sm text-white ${isProductAvailable ? "bg-neutral-400 hover:bg-neutral-400" : "bg-green-700 hover:bg-green-800"}`}
+          onClick={() => {
+            const selectedProduct = {
+              id: product.id,
+              documentId: product.documentId,
+              name: product.name,
+              basePrice: product.basePrice,
+              color: selectedColor,
+              size: selectedSize,
+              image: product.imagesByColor.find(
+                (item) => item.color === selectedColor
+              )?.images[0],
+            };
+            console.log(selectedProduct);
+          }}
+          disabled={!isProductAvailable}
+          className={classNames("py-5 text-sm text-white", {
+            "bg-neutral-400 hover:bg-neutral-400": !isProductAvailable,
+            "bg-green-700 hover:bg-green-800": isProductAvailable,
+          })}
         >
-          {isProductAvailable ? "ناموجود" : "اضافه به سبد خرید"}
-        </button>
-
-        <button
-          onClick={() => console.log("clicked second button")}
-          className="mt-4 flex flex-row justify-between px-4 py-3 text-sm text-stone-800 outline outline-1 hover:bg-stone-800 hover:text-stone-50"
-        >
-          <span>
-            <FontAwesomeIcon icon={faLocationDot} className="text-[20px]" />
-          </span>
-          مشاهده موجودی در فروشگاه حضوری
-          <span>
-            <FontAwesomeIcon icon={faChevronLeft} className="text-[20px]" />
-          </span>
+          {isProductAvailable ? "اضافه به سبد خرید" : "ناموجود"}
         </button>
       </div>
     </div>
