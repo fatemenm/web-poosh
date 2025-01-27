7f687eed5dc9a18ea7c6fcb6c8cb77cd0c7beccd
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-01-27 06:40:32 +0330

    refactor: disable navigation for the last breadcrumb item

--- app/_components/breadcrumb.tsx
+++ app/_components/breadcrumb.tsx
@@ -10,21 +10,21 @@ type BreadCrumbItem = {
 export default function BreadCrumb({ items }: { items: BreadCrumbItem[] }) {
   return (
     <div className="flex flex-row items-center gap-4 bg-stone-100 px-10 py-3 text-sm text-stone-700">
-      {items.map((item, index) => {
-        return (
+      {items.map((item, index) =>
+        items.length - 1 === index ? (
+          <span key={index}> {item.label}</span>
+        ) : (
           <Link href={item.href} key={index} className="flex flex-row gap-4">
             {item.label}
-            {items.length - 1 !== index && (
-              <span>
-                <FontAwesomeIcon
-                  className="text-[12px] text-stone-500"
-                  icon={faChevronLeft}
-                />
-              </span>
-            )}
+            <span>
+              <FontAwesomeIcon
+                className="text-[12px] text-stone-500"
+                icon={faChevronLeft}
+              />
+            </span>
           </Link>
-        );
-      })}
+        )
+      )}
     </div>
   );
 }
