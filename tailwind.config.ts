52162ecfb6cafc6a988bcaf043b3bede321a0557
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-02-05 16:47:26 +0330

    feat: define animation utility classes for accordion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,20 @@ const config: Config = {
       cursor: {
         zoomIn: "url('/zoom-in.png'), auto",
       },
+      keyframes: {
+        slideDown: {
+          from: { height: "0px" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0px" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown 500ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 500ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
     },
   },
   plugins: [],
