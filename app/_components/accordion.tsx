98870939bffdde8d9b705d7299dcd09d4f3f7d22
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-02-12 16:35:18 +0330

    refactor: make Accordion expandable for future items

--- app/_components/accordion.tsx
+++ app/_components/accordion.tsx
@@ -1,31 +1,53 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as RadixAccordion from "@radix-ui/react-accordion";
+import classNames from "classnames";
 import * as React from "react";
 
 export default function Accordion({
-  buttonText,
-  children,
+  triggerButtonText,
+  items,
+  triggerButtonClass,
+  defaultOpenItem = "",
 }: {
-  buttonText: string;
-  children: React.ReactNode;
+  triggerButtonText: string[];
+  items: React.ReactNode[];
+  triggerButtonClass: string;
+  defaultOpenItem?: string;
 }) {
   return (
-    <RadixAccordion.Root type="single" defaultValue="item-1" collapsible>
-      <RadixAccordion.Item className="group" value="item-1">
-        <RadixAccordion.Header>
-          <RadixAccordion.Trigger className="flex w-full items-center justify-between bg-white px-3 py-2 text-sm font-light text-stone-700 hover:bg-gray-50">
-            {buttonText}
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className="transition-transform duration-500 group-data-[state=open]:rotate-180"
-            />
-          </RadixAccordion.Trigger>
-        </RadixAccordion.Header>
-        <RadixAccordion.Content className="data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown mt-4 overflow-hidden px-5">
-          {children}
-        </RadixAccordion.Content>
-      </RadixAccordion.Item>
+    <RadixAccordion.Root
+      type="single"
+      defaultValue={defaultOpenItem}
+      collapsible
+    >
+      {items.map((item, index) => {
+        return (
+          <RadixAccordion.Item
+            className="group"
+            value={`item-${index}`}
+            key={index}
+          >
+            <RadixAccordion.Header>
+              <RadixAccordion.Trigger
+                className={classNames(
+                  "flex w-full items-center justify-between bg-white px-3 py-2 font-light text-stone-700 hover:bg-gray-50",
+                  triggerButtonClass
+                )}
+              >
+                {triggerButtonText[index]}
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className="transition-transform duration-500 group-data-[state=open]:rotate-180"
+                />
+              </RadixAccordion.Trigger>
+            </RadixAccordion.Header>
+            <RadixAccordion.Content className="mt-4 overflow-hidden px-5 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown">
+              {item}
+            </RadixAccordion.Content>
+          </RadixAccordion.Item>
+        );
+      })}
     </RadixAccordion.Root>
   );
 }
