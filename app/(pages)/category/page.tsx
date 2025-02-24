ed68d8d139235b0ae21aafbc811197b60896a554
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-02-24 07:54:51 +0330

    feat: add category page

---
+++ app/(pages)/category/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center bg-slate-50">
+      All products categorized under the navigation bar links will be displayed
+      here.
+    </div>
+  );
+}
