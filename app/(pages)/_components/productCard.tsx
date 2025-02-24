eb4c3dd8d919b12ba58d0cafcc60e4ba40402878
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-02-24 15:03:56 +0330

    feat: add animated transition for product image swap

--- app/(pages)/_components/productCard.tsx
+++ app/(pages)/_components/productCard.tsx
@@ -13,38 +13,63 @@ import ProductModal from "./productModal";
 export default function ProductCard({
   product,
   hoverMode,
+  isModelViewActive,
 }: {
   product: ProductModel;
-  hoverMode: "image-only" | "full-hover" | "none";
+  hoverMode: "image-hover" | "full-hover";
+  isModelViewActive: boolean;
 }) {
   const [isImageHovered, setIsImageHovered] = useState<boolean>(false);
   const [isProductModalOpen, setIsProductModalOpen] = useState<boolean>(false);
   const availableColors = product.getAvailableColors();
   const availableSizes = product.getAvailableSizes();
+  const frontImage = product.getImagesByColor(availableColors[0].name)[0];
+  const modelImage = product.getImagesByColor(availableColors[0].name)[1];
 
-  const defaultImage =
-    isImageHovered || hoverMode === "none"
-      ? product.getImagesByColor(availableColors[0].name)[1]
-      : product.getImagesByColor(availableColors[0].name)[0];
+  function handleMouseOver() {
+    setIsImageHovered(true);
+  }
+  function handleMouseOut() {
+    setIsImageHovered(false);
+  }
+
+  const productCardImage = (
+    <Link
+      onMouseOver={!isModelViewActive ? handleMouseOver : undefined}
+      onMouseOut={!isModelViewActive ? handleMouseOut : undefined}
+      href={`${nextServerUrl}/products/${product.data.documentId}`}
+      className="relative block"
+    >
+      <Image
+        className={`absolute inset-0 transition-opacity duration-500 ${
+          isImageHovered || isModelViewActive ? "opacity-0" : "opacity-100"
+        }`}
+        src={apiBaseUrl + frontImage.url}
+        alt={frontImage.alternativeText}
+        width={frontImage.width}
+        height={frontImage.height}
+      />
+      <Image
+        className={`transition-opacity duration-500 ${
+          isImageHovered || isModelViewActive ? "opacity-100" : "opacity-0"
+        }`}
+        src={apiBaseUrl + modelImage.url}
+        alt={modelImage.alternativeText}
+        width={modelImage.width}
+        height={modelImage.height}
+      />
+    </Link>
+  );
+
   return (
     <div className="group flex flex-col gap-4">
       {hoverMode === "full-hover" && (
         <div
           className="relative"
-          onMouseOver={() => setIsImageHovered(true)}
-          onMouseOut={() => setIsImageHovered(false)}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
         >
-          <Link
-            href={`${nextServerUrl}/products/${product.data.documentId}`}
-            className=""
-          >
-            <Image
-              src={apiBaseUrl + defaultImage.url}
-              alt={defaultImage.alternativeText}
-              width={defaultImage.width}
-              height={defaultImage.height}
-            />
-          </Link>
+          {productCardImage}
           <button
             onClick={() => setIsProductModalOpen(true)}
             className="invisible absolute bottom-4 left-0 right-0 m-auto flex w-4/5 flex-row items-center justify-center gap-2 bg-stone-800 bg-opacity-70 px-4 py-3 text-sm text-white hover:bg-opacity-85 group-hover:visible"
@@ -54,30 +79,7 @@ export default function ProductCard({
           </button>
         </div>
       )}
-      {hoverMode === "image-only" && (
-        <Link
-          onMouseOver={() => setIsImageHovered(true)}
-          onMouseOut={() => setIsImageHovered(false)}
-          href={`${nextServerUrl}/products/${product.data.documentId}`}
-        >
-          <Image
-            src={apiBaseUrl + defaultImage.url}
-            alt={defaultImage.alternativeText}
-            width={defaultImage.width}
-            height={defaultImage.height}
-          />
-        </Link>
-      )}
-      {hoverMode === "none" && (
-        <Link href={`${nextServerUrl}/products/${product.data.documentId}`}>
-          <Image
-            src={apiBaseUrl + defaultImage.url}
-            alt={defaultImage.alternativeText}
-            width={defaultImage.width}
-            height={defaultImage.height}
-          />
-        </Link>
-      )}
+      {hoverMode === "image-hover" && productCardImage}
       <div className="flex flex-col gap-3">
         {/* product name */}
         <Link
