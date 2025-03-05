8219696a081b9e038e4c318efc1bb42123fb45f6
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-03-05 12:48:28 +0330

    feat: support non-relative URLs

--- app/_components/promoBanner.tsx
+++ app/_components/promoBanner.tsx
@@ -9,7 +9,10 @@ export default function PromoBanner({ data }: { data: PromoBanner }) {
     <div className="box-border flex max-h-10 min-h-10 w-full flex-row items-center justify-end bg-stone-800 p-2 text-sm font-light text-white">
       <div className="flex basis-1/3 flex-row justify-center gap-4">
         {promoMessage}
-        <Link href={nextServerUrl + ctaUrl} className="underline">
+        <Link
+          href={ctaUrl.startsWith("/") ? nextServerUrl + ctaUrl : ctaUrl}
+          className="underline"
+        >
           {ctaText}
         </Link>
       </div>
