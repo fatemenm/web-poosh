bcd5ab5e37e725a72c87b780725f365ea8c5237f
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-05-12 14:39:11 +0330

    feat(login): extract AuthTabs from AuthModal and use in Login page

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import * as Form from "@radix-ui/react-form";
-import * as Tabs from "@radix-ui/react-tabs";
-import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
-import Link from "next/link";
-import { useState } from "react";
-import { useEffect } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-
-import { useAuth } from "@/_lib/context/authContext";
+import AuthTabs from "../_components/authTabs";
 
 export default function Page() {
-  const [activeTab, setActiveTab] = useState<string>("sign-in");
-  const { handleSignIn, handleSignUp } = useAuth();
   return (
-    <div>
-      <h1 className="p-10 text-lg">this is authModal</h1>
-      <br />
+    <div className="my-20 flex min-h-max justify-center">
+      <div className="mx-0 basis-1/4 rounded-sm border bg-white p-2">
+        <AuthTabs />
+      </div>
     </div>
   );
 }
