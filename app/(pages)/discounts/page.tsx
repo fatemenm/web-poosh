c82fc67c0092354eba3bc1c34be504f8b2893aaa
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-05-14 10:51:06 +0330

    feat: add discount page

---
+++ app/(pages)/discounts/page.tsx
@@ -0,0 +1,12 @@
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect } from "react";
+
+export default function Page() {
+  return (
+    <div className="mx-auto mb-10 flex w-10/12 flex-col gap-8 border-t pt-8">
+      <h4 className="text-center text-lg font-normal">تخفیف ها</h4>
+      <div className="flex flex-col gap-3"></div>
+    </div>
+  );
+}
