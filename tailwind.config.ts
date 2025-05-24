5990d31263a18bd091bb3b5d5d1d74a39dcdbe03
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-05-24 09:44:55 +0330

    feat(tailwind): add custom 'xs' screen size at 480px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+      },
       fontFamily: {
         persian: ["var(--font-vazirmatn)"],
         english: ["var(--font-roboto)"],
