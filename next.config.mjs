f03490dba4f12712a97d996638a47e234d7b69b3
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-06-30 10:51:13 +0330

    feat: configure image hostname and port in environment files

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
+        hostname: process.env.API_HOSTNAME,
+        port: process.env.API_PORT,
         pathname: "/uploads/**",
       },
     ],
