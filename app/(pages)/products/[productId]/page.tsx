06029fc6fa403cba0a47cde0cf702ec6cbf75c6d
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-07-01 17:35:59 +0330

    feat: implement static route generation using generateStaticParams and product IDs

--- app/(pages)/products/[productId]/page.tsx
+++ app/(pages)/products/[productId]/page.tsx
@@ -3,11 +3,19 @@ import ProductsSlider from "@/components/slider/ProductsSlider";
 import ProductView from "@/features/product/ProductView";
 import { getProductById, getProducts } from "@/lib/data";
 
-export default async function Product({
+export async function generateStaticParams() {
+  const products = (await getProducts()).products;
+  return products.map((product) => ({
+    productId: product.data.documentId,
+  }));
+}
+
+export default async function Page({
   params,
 }: {
   params: { productId: string };
 }) {
+  console.log(params.productId);
   const product = await getProductById(params.productId);
   const products = (await getProducts()).products;
 
